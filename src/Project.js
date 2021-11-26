cc001cfa11a386ff7f1091f3e7853a6da0ab704f
vineeth2 <vrk7@gatech.edu>
2021-11-26 13:37:25 -0500

    added title and add functionality

---
+++ src/Project.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Project extends Component {
+  render() {
+    return (
+      <main>
+        <p>Modal</p>
+        <button> Open </button>
+      </main>
+    );
+  }
+}
+
+export default Project;
